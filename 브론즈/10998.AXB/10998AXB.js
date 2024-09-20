e7598db70f6efea26358bf6702ecdd2ba824c305
장정안 <wjddks96@naver.com>
2024-09-21 01:09:59 +0900

    10998번 A X B

---
+++ 브론즈/10998.AXB/10998AXB.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+const stdin = fs
+  .readFileSync("input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+function solution() {
+  const [a, b] = stdin;
+  console.log(a * b);
+}
+
+solution();
